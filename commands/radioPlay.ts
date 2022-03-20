f414b766ce51c18fbfef69fddaf4fc8829805cc4
NANDIorg <super.egor.mamonov@yandex.ru>
2022-03-21 01:31:37 +0700

    fix rp

--- commands/radioPlay.ts
+++ commands/radioPlay.ts
@@ -19,7 +19,6 @@ export default {
     description : "Включить радио",
     aliases : ['rp'],
     slash : true,
-    testOnly : true,
     options : [
         {
             name : "voicechannle",
